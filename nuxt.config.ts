99b4a49b6efa493201779ae336d67790dd17b558
Shreyas Rama <shreyas.rama1@gmail.com>
2025-07-16 16:46:30 +1200

    Add antfu/eslint-config, vscode settings, pre-commit checks, GH action for linting (#1)
    * add antfu eslint/vscode settings
    
    * add husky and lint-staged for pre-commit
    
    * add github action for linting
    
    * fix lint workflow to install pnpm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/eslint",
+  ],
+
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+});
