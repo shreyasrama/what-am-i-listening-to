7824dca77c7247896679db2d1138c56271d8764e
shreyasrama <shreyas.rama1@gmail.com>
2025-07-16 19:20:26 +1200

    add basic vue components and folder structure

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container mx-auto text-center">
+    <h1 class="text-5xl">
+      What am I listening to?
+    </h1>
+
+    <CurrentlyPlaying />
+
+    <RecentlyPlayed />
+  </div>
+</template>
