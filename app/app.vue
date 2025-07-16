aa523c0c4698963fc1481e84d7a251a851ea5fad
Shreyas Rama <shreyas.rama1@gmail.com>
2025-07-16 19:22:19 +1200

    scaffold app with basic vue components and folder structure (#3)

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,9 @@
+<script setup lang="ts">
+import { NuxtLayout } from "#components";
+</script>
+
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-secondary">
-      View Map <Icon name="tabler:map" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ app/components/recently-played.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h2 class="text-xl">
+      Recently played:
+    </h2>
+  </div>
+</template>
