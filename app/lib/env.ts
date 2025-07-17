39f6b4c47c312426f31b15e1edd7be3bd4851f8e
Shreyas Rama <shreyas.rama1@gmail.com>
2025-07-18 11:49:18 +1200

    add basic Spotify auth flow/caching and make API call (#6)

--- app/lib/env.ts
+++ app/lib/env.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 
 const EnvSchema = z.object({
   NODE_ENV: z.string(),
+  SPOTIFY_REFRESH_TOKEN: z.string(),
+  SPOTIFY_CLIENT_CREDENTIALS: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
---
+++ app/types/spotify.d.ts
@@ -0,0 +1,19 @@
+export type AccessTokenResponse = {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  refresh_token: string;
+  scope: string;
+};
+
+export type AccessTokenState = {
+  access_token_response: AccessTokenResponse;
+  added_ts: number;
+};
+
+export type CurrentlyPlayingTrack = {
+  is_playing: boolean;
+  item: {
+    name: string;
+  };
+};
